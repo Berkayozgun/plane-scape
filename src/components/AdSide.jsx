bab66f02f5bd04e3f19b97f5ae76ddbb9b8d8756
Berkayozgun <berkayozgun001@gmail.com>
2024-09-22 14:23:25 +0300

    Refactor AdSide component to add images for car rental, hotel booking, and travel packages

--- src/components/AdSide.jsx
+++ src/components/AdSide.jsx
@@ -1,17 +1,28 @@
 import React from "react";
+import CarRental from "../assets/car.jpg";
+import HotelBooking from "../assets/hotel.jpg";
+import TravelPackages from "../assets/travel.jpg";
 
 function AdSide() {
   return (
     <div className='flex flex-col w-2/12 h-full justify-between gap-6'>
-      <div className='flex flex-col h-full shadow-2xl rounded-xl gap-6'>
-        Car Rental
+      <div className='flex flex-col h-full shadow-2xl rounded-xl gap-6 object-cover w-full '>
+        <img
+          className='flex flex-col h-full shadow-2xl rounded-xl gap-6 object-cover w-full'
+          src={CarRental}
+          alt='Car photo'
+        ></img>
       </div>
-      <div className='flex flex-col  h-full shadow-2xl rounded-xl gap-6'>
-        Hotel Booking
-      </div>
-      <div className='flex flex-col  h-full shadow-2xl rounded-xl gap-6'>
-        Travel Packages
-      </div>
+      <img
+        className='flex flex-col h-full shadow-2xl rounded-xl gap-6 object-cover w-full'
+        src={HotelBooking}
+        alt='Hotel photo'
+      />
+      <img
+        className='flex flex-col h-full shadow-2xl rounded-xl gap-6 object-cover w-full'
+        src={TravelPackages}
+        alt='Travel photo'
+      />
     </div>
   );
 }
